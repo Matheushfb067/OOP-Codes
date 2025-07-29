8f3ed2e450f39a0a4ed830c979c77d23fd423798
Matheus Henrique <matheushenrihfg0@gmail.com>
2025-07-29 14:35:57 -0300

    Fist commit Fist code JAVA

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+
+        System.out.println("O valor de z é: " + z);
+        //System.out.println("O valor de z é: " + y + z);//Concatena ao em vez de somar!
+        //System.out.println("O valor de z é: " + (y + z));//Correto!
+    }
+}
