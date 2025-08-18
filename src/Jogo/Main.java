e8285987a381751985e3eab050eb69ef4ae80f6b
Matheus Henrique <matheushenrihfg0@gmail.com>
2025-08-18 18:00:35 -0300

    add jogo

---
+++ src/Jogo/Main.java
@@ -0,0 +1,15 @@
+package Jogo;
+
+public class Main {
+    public static void main(String[] args) {
+        Personagem personagem = new Personagem();
+        Arma arma = new Arma();
+
+        personagem.nome = "Matheus";
+        personagem.pontos = 100;
+
+        personagem.arma
+
+
+    }
+}
